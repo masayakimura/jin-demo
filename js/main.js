a3b297f05f63bf6d6af84921680f3c36783089e8
masayakimura <masa_kuanoo@live.jp>
2023-01-01 16:12:28 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.burger-btn').on('click', function() {
+    $('.nav-menu').fadeToggle(300);
+    $(this).toggleClass('cross');
+    $('body').toggleClass('noscroll');
+});
+
+
+
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
